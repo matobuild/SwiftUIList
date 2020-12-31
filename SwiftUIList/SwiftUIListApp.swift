7bddb142a88d1e313b5fdcd738dc8c999a60d7e6
Your mato <you@example.com>
2020-12-31 15:22:31 +0800

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by kittawat phuangsombat on 2020/12/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
